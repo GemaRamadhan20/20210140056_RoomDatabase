87921e32334c35ba6c5d6a871cf56aed8393d80c
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-05 16:46:52 +0700

    Created HomeScreen composable function with navigation and viewModel functionality

--- app/src/main/java/com/example/repository/ui/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/repository/ui/Halaman/HalamanHome.kt
@@ -1,9 +1,22 @@
 package com.example.repository.ui.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.repository.R
+import com.example.repository.model.HomeViewModel
+import com.example.repository.model.PenyediaViewModel.Factory
 import com.example.repository.navigasi.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = Factory)
+){}
