ec9c1f0a749effb8cfa2fc1858676e497aabfcc7
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-05 16:45:14 +0700

    Added DestinasiHome implementing DestinasiNavigasi interface

---
+++ app/src/main/java/com/example/repository/ui/Halaman/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.example.repository.ui.Halaman
+
+import com.example.repository.R
+import com.example.repository.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
