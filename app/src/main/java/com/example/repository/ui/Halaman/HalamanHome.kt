87d9e41d7d43830421d0050486e76dfef0e6e632
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-06 13:59:08 +0700

    Menambahkan FloatingActionButton dan BodyHome composable untuk menampilkan daftar siswa di HomeScreen

--- app/src/main/java/com/example/repository/ui/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/repository/ui/Halaman/HalamanHome.kt
@@ -1,14 +1,25 @@
 package com.example.repository.ui.Halaman
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.repository.R
+import com.example.repository.data.Siswa
 import com.example.repository.model.HomeViewModel
 import com.example.repository.model.PenyediaViewModel.Factory
 import com.example.repository.navigasi.DestinasiNavigasi
@@ -34,3 +45,31 @@ fun HomeScreen(
                 canNavigateBack = false,
                 scrollBehavior = scrollBehavior)
 },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        }
+    ){
+            innerPadding ->
+        val uiStateSiswa by viewModel.homeUiState.collectAsState()
+        BodyHome(
+            itemSiswa = uiStateSiswa.listSiswa,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize())
+    }
+}
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+){}
+
