b35f719d9d586a47e3a4d262f1c6a777f2cce5fd
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-06 13:54:05 +0700

    Implemented TopAppBar with scroll behavior in Scaffold for DestinasiHome

--- app/src/main/java/com/example/repository/ui/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/repository/ui/Halaman/HalamanHome.kt
@@ -1,8 +1,12 @@
 package com.example.repository.ui.Halaman
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.repository.R
 import com.example.repository.model.HomeViewModel
@@ -19,4 +23,14 @@ fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel(factory = Factory)
-){}
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior)
+},
