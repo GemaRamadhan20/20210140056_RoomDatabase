e93a79e49edc259f5b9a5c9a4e88184ecea9ca50
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-06 14:20:22 +0700

    Objek DestinasiEntry: route untuk item_entry, titleRes mengacu pada string resource entry_siswa

---
+++ app/src/main/java/com/example/repository/ui/Halaman/HalamanEntry.kt
@@ -0,0 +1,9 @@
+package com.example.repository.ui.Halaman
+
+import com.example.repository.R
+import com.example.repository.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
